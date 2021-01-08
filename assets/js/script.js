ecb28aa41ec89a8d5f74a09f6e8f67828fe57b4e
Matt Autrey <mautrey17@gmail.com>
2021-01-07 22:05:14 -0500

    updated notes and readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 //Global Variables
 var displayHours = ["9 AM", "10 AM", "11 AM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM"];
 var valueHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var fixedEntries = [];
+var localEntries = JSON.parse(localStorage.getItem("entries"));
 
 //Display Current Time
 $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
 
-//create empty objects for entries
-var fixedEntries = [];
-
-var localEntries = JSON.parse(localStorage.getItem("entries"));
-    console.log(localEntries);
-
+//Check if there are existing entries
 var checkStorage = function(){
-    // var localEntries = JSON.parse(localStorage.getItem("entries"));
-    console.log("localEntries");
     if (localEntries){
         fixedEntries = localEntries;
-        console.log("this works")
     }
     else{
         for(var i = 0; i<valueHours.length; i++){
@@ -29,10 +23,10 @@ var checkStorage = function(){
     }
 }
 checkStorage();
-console.log(fixedEntries);
 
 //create time blocks
 for(var i=0; i<displayHours.length; i++){
+
     //create HTML for time block
     var timeBlock = $("<div>").attr("class", "time-block");
     var row = $("<div>").attr("class", "row");
@@ -40,17 +34,15 @@ for(var i=0; i<displayHours.length; i++){
     var colHour = $("<div>").attr("class", "col-md-1");
     var hour = $("<div>").attr("class", "hour").text(displayHours[i]);
     colHour.append(hour);
+
     //create HTML for text entry
     var colEntry = $("<div>").attr("class", "col-md-10");
-    
-
     var entry = $("<textarea>").attr("data-time", valueHours[i]);
     entry.text(fixedEntries[i].sampleText);
 
     //color code entries based on time of day
-    // console.log(entry.attr("data-time"))
     var setTime = parseInt(entry.attr("data-time"));
-    // console.log(setTime);
+
     if(setTime < parseInt(dayjs().format("H"))){
         entry.attr("class", "past");
     }
@@ -70,31 +62,21 @@ for(var i=0; i<displayHours.length; i++){
     btn.append(img);
     colBtn.append(btn);
     
+    //add HTML to page
     row.append(colHour, colEntry, colBtn)
     timeBlock.append(row);
     $(".container").append(timeBlock);
 }
 
-var savedEntries = [];
-
+//Function for clicking save button
 $(".saveBtn").on("click", function(){
-    // var newEntry = {
-    //     "timeSlot": this.getAttribute("data-timeBtn"),
-    //     "text": $(this).parent().prev().children().val()
-    // }
 
+    //set data to proper value and target correct entry
     var whichOne = this.getAttribute("data-timeBtn");
     whichOne -= 9;
     fixedEntries[whichOne].sampleText = $(this).parent().prev().children().val();
 
-    console.log(fixedEntries);
-    
-    // fixedEntries.push(newEntry);
+    // Add to local storage
     localEntries = JSON.stringify(fixedEntries);
     localStorage.setItem("entries", localEntries);
-    console.log(localEntries);
 })
-
-
-
-// console.log(fixedEntries);
